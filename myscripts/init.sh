62ad1c7c3ce0713d9ec447b25279a2ebe9172c71
pawel <pawelzell@gmail.com>
2020-03-15 12:10:24 +0100

    update scripts

--- myscripts/init.sh
+++ myscripts/init.sh
@@ -1,6 +1,9 @@
 # If you are using cbtool with kind, suffix should be _kind
-SUF=""
+SUF="$1"
+MONGO_CONFIG="mongodb${SUF}.conf"
+echo "Script first argument is an suffix: ${SUF}"
+echo "Mongodb configs: ${MONGO_CONFIG}"
 
 sudo redis-server /etc/redis/redis.conf &
-sudo cp mongodb${SUF}.conf /etc/mongodb.conf &&
+sudo cp $MONGO_CONFIG /etc/mongodb.conf &&
 sudo mongod --config /etc/mongodb.conf &
--- myscripts/scp_exp_data.sh
+++ myscripts/scp_exp_data.sh
@@ -4,6 +4,8 @@ if [[ $# -ne 1 ]]; then
 fi
 
 echo "Copy exp data for $1"
-DIR=~/cbtool/data/$1
+DIR=/home/pziecik/cbtool/data/$1
 HOST="baati"
-scp -o 'ProxyJump students' -r $HOST:$DIR .
+scp -o 'ProxyJump students' -r $HOST:$DIR . || exit 1
+cd $1
+gnome-open 008_vm_app_throughput_vs_time_plot_redis_ycsb.pdf &
