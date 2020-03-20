35f77c5cc7dc77258546586df9fab4d3364fefca
pawel <pawelzell@gmail.com>
2020-03-20 10:48:20 +0100

    cp exp data small change

--- myscripts/scp_exp_data.sh
+++ myscripts/scp_exp_data.sh
@@ -6,9 +6,7 @@ kEXPID=$1
 
 echo "Copy exp data for $kEXPID"
 DIR=/home/pziecik/cbtool/data/$kEXPID
-RESOURCES_DIR=/home/pziecik/cbtool/myscripts/resources
 HOST="baati"
 scp -o 'ProxyJump students' -r $HOST:$DIR . || exit 1
-scp -o 'ProxyJump students' -r $HOST:$RESOURCES_DIR $kEXPID || exit 1
 cd $kEXPID
 gnome-open 008_vm_app_throughput_vs_time_plot_redis_ycsb.pdf &
