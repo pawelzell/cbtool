53fd88c9ad8d12dff2736364af64dbb43c6b594e
pawel <pawelzell@gmail.com>
2020-03-20 09:30:35 +0100

    scp_exp_data.sh update

--- myscripts/scp_exp_data.sh
+++ myscripts/scp_exp_data.sh
@@ -1,11 +1,14 @@
 if [[ $# -ne 1 ]]; then
-	echo "Illegal number of parameters, got $# expected 1"
+	echo "Usage $0 <expid>"
 	exit 1
 fi
+kEXPID=$1
 
-echo "Copy exp data for $1"
-DIR=/home/pziecik/cbtool/data/$1
+echo "Copy exp data for $kEXPID"
+DIR=/home/pziecik/cbtool/data/$kEXPID
+RESOURCES_DIR=/home/pziecik/cbtool/myscripts/resources
 HOST="baati"
 scp -o 'ProxyJump students' -r $HOST:$DIR . || exit 1
-cd $1
+scp -o 'ProxyJump students' -r $HOST:$RESOURCES_DIR $kEXPID || exit 1
+cd $kEXPID
 gnome-open 008_vm_app_throughput_vs_time_plot_redis_ycsb.pdf &
