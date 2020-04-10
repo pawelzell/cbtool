783b3b0205e997f1735aec6922e08c2a6d74436d
pawel <pawelzell@gmail.com>
2020-04-10 09:56:04 +0200

    fix clear influxdb.sh

--- myscripts/clear_influxdb.sh
+++ myscripts/clear_influxdb.sh
@@ -1,7 +1,5 @@
-kDIR="cleanup"
 kINFLUX_POD=`kubectl get pods | awk '/^influx/ {print $1}'`
 echo "Will clear influxdb database for pod: $kINFLUX_POD"
 
-kubectl exec --namespace default influxdb-68999c6797-5bqnq -- bash -c "mkdir -p $kDIR"
-kubectl cp helper_clear_influxdb.sh $kINFLUX_POD:$kDIR #&&
-kubectl exec --namespace default $kINFLUX_POD -- bash -c "cd $kDIR && ./helper_clear_influxdb.sh"
+kubectl cp helper_clear_influxdb.sh $kINFLUX_POD:/ #&&
+kubectl exec --namespace default $kINFLUX_POD -- bash -c "cd / && ./helper_clear_influxdb.sh"
