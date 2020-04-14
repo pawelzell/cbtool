e836683583a8831c1415fdf8715d666750cd2d1f
pawel <pawelzell@gmail.com>
2020-04-14 10:55:58 +0200

    First notebook for data analysis; scp_data update script

--- myscripts/scp_exp_data.sh
+++ myscripts/scp_exp_data.sh
@@ -1,12 +1,43 @@
 if [[ $# -ne 1 ]]; then
-	echo "Usage $0 <expid>"
+	echo "Usage $0 <remotehost>"
 	exit 1
 fi
-kEXPID=$1
+kREMOTEUSER=pziecik
+kREMOTEHOST=$1
 
-echo "Copy exp data for $kEXPID"
-DIR=/home/pziecik/cbtool/data/$kEXPID
-HOST="baati"
-scp -o 'ProxyJump students' -r $HOST:$DIR . || exit 1
-cd $kEXPID
-gnome-open 008_vm_app_throughput_vs_time_plot_redis_ycsb.pdf &
+kLOCAL_DIR="data"
+kTMP_DIR="../data"
+
+kSCRIPT_BASE_DIR=$PWD
+kREMOTE_DIR="/home/$kREMOTEUSER/cbtool/data"
+kEXPS=`ssh -J students baati ls /home/$kREMOTEUSER/cbtool/data`
+for kEXP in $kEXPS; do
+  if [[ ! -e $kLOCAL_DIR/$kEXP ]]; then
+		echo "Copy $kEXP"
+		# 1 Copy
+		kDIR="$kREMOTE_DIR/$kEXP"
+		scp -o 'ProxyJump students' -r $kREMOTEHOST:$kDIR $kTMP_DIR
+
+		# 2 Plot
+		cd $kTMP_DIR/$kEXP
+		kRES="$?"
+		if [[ $kRES -ne 0 ]]; then
+			echo "ASSERT cd $kTMP_DIR/$kEXP - FAILED"
+			exit 1
+		fi 
+		./plot.sh
+		cd $kSCRIPT_BASE_DIR
+		if [[ $PWD != $kSCRIPT_BASE_DIR ]]; then
+			echo "ASSERT UNEXPECTED DIR $PWD exitting" 
+			exit 1
+		fi 
+
+		# 3 Move
+		mv $kTMP_DIR/$kEXP $kLOCAL_DIR
+	else
+		echo "Skipping $kEXP - directory already exists"
+	fi
+done
+
+
+
