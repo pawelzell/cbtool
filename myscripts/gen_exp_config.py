65b4f55e7e8eaf6cca1682352395ae40aefffeb6
pawel <pawelzell@gmail.com>
2020-04-21 14:33:07 +0200

    gen_exp_config update

--- myscripts/gen_exp_config.py
+++ myscripts/gen_exp_config.py
@@ -1,5 +1,2 @@
-cpu_limits = [
-  ("redis_ycsb", "ycsb", 2371),
-  ("redis_ycsb", "redis", 466),
-]
+cpu_limits = [('redis_ycsb', 'ycsb', 2370), ('redis_ycsb', 'redis', 465), ('hadoop', 'hadoopmaster', 1313), ('hadoop', 'hadoopslave', 2238), ('linpack', 'linpack', 3593), ('wrk', 'wrk', 1784), ('wrk', 'apache', 2834)]
 
