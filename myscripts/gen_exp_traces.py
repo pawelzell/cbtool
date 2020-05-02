38025e2f4483c8bd6f15e6969da2dd54f96bab86
pawel <pawelzell@gmail.com>
2020-05-02 10:19:11 +0200

    Change gen_exp_traces to set number of hadoop slaves

--- myscripts/gen_exp_traces.py
+++ myscripts/gen_exp_traces.py
@@ -2,8 +2,11 @@ import sys
 import os
 import gen_exp_config as config
 
+hadoop_slave_no = 2
+
 cpu_requests = "typealter {} {}_cpu_requests=1m\n"
 cpu_limits = "typealter {} {}_cpu_limits={}m\n"
+hadoop_sut = "typealter hadoop sut=hadoopmaster->{}_x_hadoopslave\n"
 
 prefix = \
 """cldattach kub TESTKUB
@@ -12,7 +15,6 @@ vmcattach all
 vmclist
 
 cldalter ai_defaults run_limit=100000000
-typealter hadoop sut=hadoopmaster->hadoopslave
 typealter hadoop load_level=1
 typealter wrk load_level=1
 typealter wrk load_duration=1
@@ -32,6 +34,20 @@ clddetach
 exit
 """
 
+def set_hadoop_sut(f, x, y):
+    t = None
+    if x.startswith("hadoop"):
+        t = x
+    elif y.startswith("hadoop"):
+        t = y
+    else: 
+        return
+    no_raw = t[len("hadoop"):] 
+    if not no_raw:
+        no = 1
+    else:
+        no = int(no_raw)
+    f.write(hadoop_sut.format(no))
 
 def gen_exp(x, y, nr, task_count, interval="20m"):
     basepath="../traces"
@@ -42,7 +58,9 @@ def gen_exp(x, y, nr, task_count, interval="20m"):
     filename = os.path.join(basepath, basename)
     print(f"will generate {filename} {x} {y} {nr}")
     with open(filename, "w") as f:
+        f.write(f"# {x} {y} {task_count}\n")
         f.write(prefix.format(nr, basename))
+        f.write(hadoop_sut.format(hadoop_slave_no))
         for ai_type, role, limit in config.cpu_limits:
             f.write(cpu_requests.format(ai_type, role))
             f.write(cpu_limits.format(ai_type, role, limit))
@@ -50,7 +68,6 @@ def gen_exp(x, y, nr, task_count, interval="20m"):
         for _ in range(task_count-1):
             f.write(instance.format(y, interval))
         f.write(suffix)
-
     return nr+1
 
 def main():
@@ -63,6 +80,7 @@ def main():
     for x in sys.argv[3:]:
         for y in sys.argv[3:]:
             nr = gen_exp(x, y, nr, task_count)
+    print(f"Hadoop slave number set to {hadoop_slave_no}")
 
 if __name__ == "__main__":
     main()
