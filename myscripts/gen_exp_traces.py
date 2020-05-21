316ed53eaab9d5495576d39ac29e1a49084a3ac4
pawel <pawelzell@gmail.com>
2020-05-21 14:19:30 +0200

    Add option to generate scheduler experiments

--- myscripts/gen_exp_traces.py
+++ myscripts/gen_exp_traces.py
@@ -4,8 +4,10 @@ import sys
 import os
 import pandas as pd
 resource_data_csv = "resource.csv"
+types_default = ["redis_ycsb", "wrk", "hadoop", "linpack"]
 hadoop_slave_no = 2
-max_hadoop_count = 7
+max_hadoop_count = 15
+scheduler_exp_shuffles_count = 5
 
 resource_constraints = "typealter {type} {role}_{resource}_{constraint}={value}\n"
 hadoop_sut = "typealter hadoop sut=hadoopmaster->{}_x_hadoopslave\n"
@@ -22,11 +24,26 @@ typealter wrk load_level=1
 typealter wrk load_duration=1
 """
 
+wait_cmd = \
+"""
+waitfor {}m
+"""
+
 instance = \
 """
 aiattach {}
 vmlist
 waitfor {}m
+"""
+
+instance_async = \
+"""
+aiattach {} async
+"""
+
+wait_for_all_ai_arrival = \
+"""
+waituntil AI ARRIVED={} increasing 20 7200
 """
 
 suffix = \
@@ -36,9 +53,11 @@ clddetach
 exit
 """
 
+
 def is_mixed_tasks_list_valid(tasks):
     hadoop_count = sum([1 for task in tasks if task == "hadoop"])
     return hadoop_count <= max_hadoop_count
+
 
 def get_resource_constraints():
     results = []
@@ -61,16 +80,26 @@ def get_resource_constraints():
             results.append(result)
     return results
 
-def gen_exp(expid, filename, description, tasks, interval, constraints):
+
+def gen_exp(expid, filename, description, tasks, interval, constraints, async=False):
     with open(filename, "w") as f:
         f.write(description)
         f.write(prefix.format(expid))
         f.write(hadoop_sut.format(hadoop_slave_no))
         for c in constraints:
             f.write(resource_constraints.format(**c))
-        for task in tasks:
-            f.write(instance.format(task, interval))
+        if async:
+            for task in tasks:
+                f.write(instance_async.format(task))
+            f.write(wait_for_all_ai_arrival.format(len(tasks)))
+            f.write(wait_cmd.format(interval))
+        else:
+            for task in tasks:
+                f.write(instance.format(task, interval))
         f.write(suffix)
+
+
+# Change interval - no interval in between tasks
 
 def gen_mixed_tasks_list(types, task_count, max_tries=100):
     for _ in range(max_tries):
@@ -82,6 +111,7 @@ def gen_mixed_tasks_list(types, task_count, max_tries=100):
             return tasks
     raise ValueError(f"Tried to sample a tasks list for mixed experiment and failed {max_tries} times")
 
+
 def gen_exp_mixed(types, no, task_count, interval, constraints):
     basepath="../traces"
     basename = expid = f"{no}mixed"
@@ -90,6 +120,7 @@ def gen_exp_mixed(types, no, task_count, interval, constraints):
     description = f'# mixed {",".join(tasks)}\n'
     print(f"will generate {filename}")
     gen_exp(expid, filename, description, tasks, interval, constraints)
+
 
 def gen_exp_linear(x, y, no, task_count, interval, constraints):
     basepath="../traces"
@@ -104,6 +135,19 @@ def gen_exp_linear(x, y, no, task_count, interval, constraints):
     print(f"will generate {filename} {x} {y} {no}")
     gen_exp(expid, filename, description, tasks, interval, constraints)
 
+
+def gen_exp_scheduler(types, no, task_count, interval, constraints):
+    basepath = "../traces"
+    tasks = gen_mixed_tasks_list(types, task_count)
+    for i in range(scheduler_exp_shuffles_count):
+        random.shuffle(tasks)
+        basename = expid = f"{no}scheduler{i}"
+        description = f'# scheduler {",".join(tasks)}\n'
+        filename = os.path.join(basepath, basename)
+        print(f"will generate {filename}")
+        gen_exp(expid, filename, description, tasks, interval, constraints, async=True)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Generate CBTOOL experiments traces. "\
         "Generated experiment descriptions are saved to ../traces.")
@@ -111,21 +155,42 @@ def parse_args():
         "Generated experiment ids have form '<order number><exp_type>' for consecutive "
         "order numbers. This parameter specifiy the order number to start from.")
     parser.add_argument("task_count", metavar="K", type=int, help="Number of tasks deployed in a single experiment.")
-    parser.add_argument(dest="types", nargs="+", type=str, help="List of task types. Each task type "
-        "has to be valid CBTOOL application instance type e.g. redis_ycsb, hadoop, linpack")
-    parser.add_argument("-m", dest="mode", default="linear", choices=["linear", "mixed"], help="Type of " \
+    parser.add_argument("-t", dest="types_file", type=str, default=None, help="File with list of task types (each type in a separate line. "\
+        "Every task type has to be a valid CBTOOL application instance type e.g. redis_ycsb, hadoop, linpack")
+    parser.add_argument("-m", dest="mode", default="linear", choices=["linear", "mixed", "scheduler"], help="Type of " \
         "generated experiments (default linear). 'linear' - deploys one task of type x and (N-1) tasks of type y." \
-        "`mixed` - deploys N tasks of random types, but at least from three different types if possible.")
+        "`mixed` - deploys N tasks of random types, but at least from three different types if possible." \
+        "`scheduler` - simmilar to mixed but generates multiple experiments with the same task composition but" \
+        "with different order of tasks.")
     parser.add_argument("-i", metavar="I", dest="interval", type=int, default=20, help="Number of minutes" \
         " to wait between deployment of two consecutive tasks.")
     parser.add_argument("-n", metavar="N", dest="experiment_count", type=int, default=10, help="If mixed mode " \
         "is selected, generates N different experiments.")
+    parser.add_argument("-hadoop", metavar="H", dest="max_hadoop_count", type=int, default=None, \
+        help="Prevent generation of an experiment with more than H hadoop tasks in order to avoid OOM.")
     return parser.parse_args()
+
+
+def read_task_types_from_file(types_file):
+    result = []
+    with open(types_file, "r") as f:
+        for t in f:
+            result.append(t.strip())
+    return result
+
 
 def main():
     args = parse_args()
     constraints = get_resource_constraints()
     no = args.no
+    if args.types_file:
+        types = read_task_types_from_file(args.types_file)
+    else:
+        types = types_default
+    if args.max_hadoop_count:
+        global max_hadoop_count
+        max_hadoop_count = args.max_hadoop_count
+
     if args.mode == "linear":
         for x in args.types:
             for y in args.types:
@@ -133,7 +198,11 @@ def main():
                 no += 1
     elif args.mode == "mixed":
         for _ in range(args.experiment_count):
-            gen_exp_mixed(args.types, no, args.task_count, args.interval, constraints)
+            gen_exp_mixed(types, no, args.task_count, args.interval, constraints)
+            no += 1
+    elif args.mode == "scheduler":
+        for _ in range(args.experiment_count):
+            gen_exp_scheduler(types, no, args.task_count, args.interval, constraints)
             no += 1
     else:
         print(f"Unsupported mode {args.mode}")
