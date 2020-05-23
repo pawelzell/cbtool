e95ae6c91e146614b0a5f907c5841373b5cafcc5
pawel <pawelzell@gmail.com>
2020-05-23 15:20:18 +0200

    export config to scheduler

--- myscripts/export_config_to_scheduler.sh
+++ myscripts/export_config_to_scheduler.sh
@@ -7,4 +7,4 @@ fi
 kSCHEDULER_POD=`kubectl get pods | awk '/^type-aware-scheduler/ {print $1}'`
 echo "Will copy experiment config file to: $kSCHEDULER_POD"
 
-kubectl cp $0 $kINFLUX_POD:/exp
+kubectl cp $1 $kSCHEDULER_POD:/exp
