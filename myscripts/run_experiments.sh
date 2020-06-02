1f56694653be320a762d1110ae4a4e8da9f20eee
pawel <pawelzell@gmail.com>
2020-06-02 11:35:20 +0200

    run_experiments - option to run all traces from dir

--- myscripts/run_experiments.sh
+++ myscripts/run_experiments.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-if [[ $# -lt 1 ]]; then
-  echo "Usage: $0 <expfiles>"
+if [[ ( $# -lt 2 ) || (( $1 != "-files" ) && ( $1 != "-dir" )) ]]; then
+  echo "Usage: $0 -files <expfiles from ../traces dir>"
+  echo "Usage: $0 -dir <dir with expfiles>"
   exit 1
 fi
 
-for kEXPFILE in $@; do
+if [[ $1 == "-files" ]]; then
+  kEXPDIR="../traces"
+  kEXPFILES=${@:2}
+else
+  kEXPDIR=$2
+  kEXPFILES=`ls "$2"`
+fi
+
+for kEXPFILE in ${kEXPFILES}; do
+  kEXPFILE="myscripts/$kEXPDIR/$kEXPFILE"
+  ./export_config_to_scheduler.sh ../${kEXPFILE}
   ./clear_influxdb.sh
-  kEXPFILE="traces/$kEXPFILE"
-  kIMAGES=`awk '/^aiattach/ {print $2}' ../$kEXPFILE | sort -u`
+  kIMAGES=`awk '/^aiattach/ {print $2}' ../${kEXPFILE} | sort -u`
   for kIMAGE in $kIMAGES; do
     echo "Pull image $kIMAGE"
     ./pull_images.sh $kIMAGE
   done
-  ./export_config_to_scheduler.sh ../$kEXPFILE
   cd .. || exit 1
   echo "Will run cbtool for expfile: $kEXPFILE"
-  kEXPID=`awk '/^expid/ {print}' $kEXPFILE | sed 's/expid //'`
+  kEXPID=`awk '/^expid/ {print}' ${kEXPFILE} | sed 's/expid //'`
   echo "Detected expid : $kEXPID"
-  sudo ./cb --soft_reset --trace $kEXPFILE
+  sudo ./cb --soft_reset --trace ${kEXPFILE}
   cd -
-  ./export_exp.sh $kEXPID
+  ./export_exp.sh ${kEXPID}
 done
 
